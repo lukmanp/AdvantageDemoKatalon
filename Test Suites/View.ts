afd0304d795b6d0413f516e9a7f142b418c74e2e
lintaniafina16 <lintaniafina16@gmail.com>
2023-10-10 11:32:01 +0700

    EduApp

---
+++ Test Suites/View.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>View</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>3f327a8b-cac6-4b17-acf1-b2d70d0bbdcd</testSuiteGuid>
+</TestSuiteEntity>
